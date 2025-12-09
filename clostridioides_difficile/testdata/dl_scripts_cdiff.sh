5e72cf1100245080aed33807c8fab38ab78aef60
RAHenriksen <rah@hotmail.dk>
2025-12-09 07:45:13 +0100

    added testdata set relevant for species-specific development at ssi_analysis_utility

---
+++ clostridioides_difficile/testdata/dl_scripts_cdiff.sh
@@ -0,0 +1,98 @@
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR142/ERR142064/ERR142064_1.fastq.gz # C.diff
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR142/ERR142064/ERR142064_2.fastq.gz
+
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR541/003/SRR5410473/SRR5410473_1.fastq.gz #ST11 https://www.ebi.ac.uk/ena/browser/view/PRJNA381384 
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR541/003/SRR5410473/SRR5410473_2.fastq.gz
+
+#https://www.cambridge.org/core/journals/infection-control-and-hospital-epidemiology/article/transmission-of-clostridium-difficile-from-asymptomatically-colonized-or-infected-longterm-care-facility-residents/F1E96606EA9C60A43517FA112745F4E2
+#ST1
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR291/006/SRR2915766/SRR2915766_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR291/006/SRR2915766/SRR2915766_2.fastq.gz
+#https://www.ebi.ac.uk/ena/browser/text-search?query=ST1%20difficile
+#ST2 
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR105/019/SRR10518319/SRR10518319_1.fastq.gz 
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR105/019/SRR10518319/SRR10518319_2.fastq.gz 
+# ST35 - https://www.tandfonline.com/doi/full/10.1080/22221751.2024.2408322#d1e1533 
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR105/008/SRR10518508/SRR10518508_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR105/008/SRR10518508/SRR10518508_2.fastq.gz
+#https://www.ebi.ac.uk/ena/browser/view/ERR027374
+# ST37 - Ribotype 17 
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR027/ERR027374/ERR027374_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR027/ERR027374/ERR027374_2.fastq.gz
+#https://link.springer.com/article/10.1186/s12879-024-09841-9#additional-information
+#ST2
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/004/SRR26769804/SRR26769804_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/004/SRR26769804/SRR26769804_2.fastq.gz
+#ST3
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/002/SRR26769802/SRR26769802_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/002/SRR26769802/SRR26769802_2.fastq.gz
+#ST5
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/017/SRR26769817/SRR26769817_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/017/SRR26769817/SRR26769817_2.fastq.gz
+#ST8
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/014/SRR26769814/SRR26769814_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/014/SRR26769814/SRR26769814_2.fastq.gz
+#ST17
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/015/SRR26769815/SRR26769815_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/015/SRR26769815/SRR26769815_2.fastq.gz
+#ST37
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/009/SRR26769809/SRR26769809_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/009/SRR26769809/SRR26769809_2.fastq.gz
+#ST42
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/012/SRR26769812/SRR26769812_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/012/SRR26769812/SRR26769812_2.fastq.gz
+#ST81
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/093/SRR26769793/SRR26769793_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/093/SRR26769793/SRR26769793_2.fastq.gz
+#ST183 
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/091/SRR26769791/SRR26769791_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/091/SRR26769791/SRR26769791_2.fastq.gz
+#ST185
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/003/SRR26769803/SRR26769803_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR267/003/SRR26769803/SRR26769803_2.fastq.gz
+
+#https://www.tandfonline.com/doi/10.1080/22221751.2024.2427804?url_ver=Z39.88-2003&rfr_id=ori:rid:crossref.org&rfr_dat=cr_pub%20%200pubmed#d1e1884
+#ST1 -> 18
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/056/SRR14766156/SRR14766156_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/056/SRR14766156/SRR14766156_2.fastq.gz
+#ST6
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/064/SRR14766164/SRR14766164_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/064/SRR14766164/SRR14766164_2.fastq.gz
+#ST8
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/089/SRR14766189/SRR14766189_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/089/SRR14766189/SRR14766189_2.fastq.gz
+#ST11
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/066/SRR14766166/SRR14766166_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/066/SRR14766166/SRR14766166_2.fastq.gz
+#ST14
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/068/SRR14766168/SRR14766168_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/068/SRR14766168/SRR14766168_2.fastq.gz
+#ST15
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/063/SRR14766163/SRR14766163_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/063/SRR14766163/SRR14766163_2.fastq.gz
+#ST16
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/086/SRR14766186/SRR14766186_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/086/SRR14766186/SRR14766186_2.fastq.gz
+#ST49
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/065/SRR14766165/SRR14766165_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/065/SRR14766165/SRR14766165_2.fastq.gz
+#ST92
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/080/SRR14766180/SRR14766180_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR147/080/SRR14766180/SRR14766180_2.fastq.gz
+
+# https://pmc.ncbi.nlm.nih.gov/articles/PMC8655013/#SM1
+#ST5 -> 0 or 18 or 54
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/067/SRR12520767/SRR12520767_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/067/SRR12520767/SRR12520767_2.fastq.gz
+#ST10 -> 18
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/039/SRR12520739/SRR12520739_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/039/SRR12520739/SRR12520739_2.fastq.gz
+#ST13 -> 0 or 18
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/020/SRR12520720/SRR12520720_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/020/SRR12520720/SRR12520720_2.fastq.gz
+#ST28 -> 18
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/078/SRR12520678/SRR12520678_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/078/SRR12520678/SRR12520678_2.fastq.gz
+#ST41 -> 18 
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/050/SRR12520650/SRR12520650_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/050/SRR12520650/SRR12520650_2.fastq.gz
